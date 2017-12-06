4d0e74bbff7a2229cd33ac36007105794e19f489
Pama <625706468@qq.com>
2017-12-06 09:44:24 +0800

    更新文件

--- src/html/index/index.js
+++ src/html/index/index.js
@@ -5,16 +5,14 @@ log('apple')
 const swiperH = new Swiper('.swiper-container-h', {
     mousewheel: true,
     direction: 'vertical',
-    spaceBetween: 30,
 	pagination: {
-	  el: '.swiper-pagination-h',
-	  clickable: true,
+		el: '.swiper-pagination-h',
+		clickable: true,
 	},
 })
 const swiperV = new Swiper('.swiper-container-v', {
-    spaceBetween: 30,
 	pagination: {
-	  el: '.swiper-pagination-v',
-	  clickable: true,
+		el: '.swiper-pagination-v',
+		clickable: true,
 	},
 })
