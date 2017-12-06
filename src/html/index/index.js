f939009c1f221984be85637069b74740734ea5a9
Tutu1993 <625706468@qq.com>
2017-12-07 00:01:18 +0800

    更新页面

--- src/html/index/index.js
+++ src/html/index/index.js
@@ -3,15 +3,14 @@ require('cssDir/index/index.css')
 const log = console.log.bind(console)
 
 const swiperV = new Swiper('.swiper-container-v', {
-    direction: 'vertical',
-    speed: 500,
-    roundLengths: true,
-    keyboard: true,
-    mousewheel: true,
-	followFinger: false,
-	resistanceRatio : 0,
-	touchAngle: 30,
-    longSwipesRatio: 0.3,
+	direction: 'vertical', // 竖向切换
+	speed: 500, // 切换消耗时间
+	roundLengths: true, // 宽高取整
+	slidesPerView: 'auto', // 同屏显示 slide 的数量
+	followFinger: false, // 释放时 slide 才会切换
+	resistanceRatio: 0, // 边缘抵抗力
+	touchAngle: 30, // 允许触发拖动的角度值
+	mousewheel: true, // 开启鼠标
 	pagination: {
 		el: '.swiper-pagination-v',
 		clickable: true,
@@ -20,8 +19,6 @@ const swiperV = new Swiper('.swiper-container-v', {
 const swiperH = new Swiper('.swiper-container-h', {
 	loop: true,
 	followFinger: false,
-	nested: true,
-	resistanceRatio : 0,
 	// autoplay: {
 	// 	delay: 3000,
 	// },
