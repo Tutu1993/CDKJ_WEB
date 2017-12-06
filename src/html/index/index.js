38e02fd4d344d566ad2c163d562a570a9e04b4ec
Pama <625706468@qq.com>
2017-12-06 18:36:19 +0800

    暂缓，效果很差

--- src/html/index/index.js
+++ src/html/index/index.js
@@ -1,8 +1,17 @@
 require('cssDir/index/index.css')
 
+const log = console.log.bind(console)
+
 const swiperV = new Swiper('.swiper-container-v', {
-    mousewheel: true,
     direction: 'vertical',
+    speed: 500,
+    roundLengths: true,
+    keyboard: true,
+    mousewheel: true,
+	followFinger: false,
+	resistanceRatio : 0,
+	touchAngle: 30,
+    longSwipesRatio: 0.3,
 	pagination: {
 		el: '.swiper-pagination-v',
 		clickable: true,
@@ -10,6 +19,13 @@ const swiperV = new Swiper('.swiper-container-v', {
 })
 const swiperH = new Swiper('.swiper-container-h', {
 	loop: true,
+	followFinger: false,
+	nested: true,
+	resistanceRatio : 0,
+	// autoplay: {
+	// 	delay: 3000,
+	// },
+	touchAngle: 30,
 	navigation: {
 		prevEl: '.swiper-button-prev',
 		nextEl: '.swiper-button-next',
